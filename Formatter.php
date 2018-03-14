d9de4bfab0d06a1f3e4097bedffabf38ef4cd7bc
nazishmildly <nazish@mildlyclassic.com>
2018-03-14 12:05:50 +0530

    add dummy files

---
+++ Formatter.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+	Author : Nazish Fraz
+	Email : nfraz007@gmail.com
+	Website : nfraz.co.nf
+	Date : 2018-03-15
+	Description : This file is basically for formatting the text in another format
+*/
+
+class Format
+{
+	function __construct(){
+
+	}
+
+	public static function index(){
+		return 1;
+	}
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require 'Formatter.php';
+
+echo Format::index();
+?>
